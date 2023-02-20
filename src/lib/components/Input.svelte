241c7e774cf507751f5790146234534191af9d84
qasimwarraich <qasim.warraich@gmail.com>
2023-02-20 15:04:49 +0100

    feat: Adds a frontend for the registration form

---
+++ src/lib/components/Input.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	export let value = '';
+	export let placeholder = '';
+	export let id: string;
+	export let label: string;
+	export let type = 'text';
+	export let disabled = false;
+	export let required = false;
+	export let isCheckBox = false;
+
+	let normalField = 'border-theme-1 border-2 border-solid rounded-none w-full max-w-lg p-1';
+	let checkBoxField = 'ml-2 max-w-lg';
+
+	if (type == 'checkbox') {
+		isCheckBox = true;
+	}
+</script>
+
+<div class="w-full max-w-lg mb-2">
+	<label for={id} class="font-bold pb-1">
+		<span class="font-bold">{label}</span>
+	</label>
+	<input
+		class={isCheckBox ? checkBoxField : normalField}
+		{type}
+		{placeholder}
+		{required}
+		{disabled}
+		{id}
+		name={id}
+		{value}
+	/>
+</div>
--- src/routes/registration/+page.svelte
+++ src/routes/registration/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import rad from '$lib/images/radmaskottchen.png';
+	import Input from '$lib/components/Input.svelte';
 </script>
 
 <svelte:head>
@@ -10,4 +11,34 @@
 <h1 class="flex flex-col items-center justify-center text-5xl text-theme-1 font-semibold">
 	<img class="h-96" src={rad} alt="SUICMC" />
 	Coming soon!
+	<h2 class="p-2 font-bold">
+		This form is currently under testing, <span class="underline ">DO NOT</span> use
+	</h2>
 </h1>
+
+<div class="flex flex-col w-full h-full p-2">
+	<div class="w-full">
+		<form action="?/create" method="POST" class="flex flex-col w-full items-center">
+			<span class="p-2">
+				<Input id="fname" label="First Name" required={true} />
+				<Input id="lname" label="Last Name" required={true} />
+				<Input id="nname" label="Nick Name" required={true} />
+				<Input id="tname" label="Team/Crew Name" />
+				<Input id="city" label="City" />
+				<Input type="email" id="email" label="Email Address" required={true} />
+				<Input
+					type="number"
+					placeholder="420"
+					id="race_number"
+					label="Race Number"
+					required={true}
+				/>
+				<Input type="checkbox" id="safety" label="Safety First?" />
+				<Input type="checkbox" id="housing" label="Do you require housing?" />
+				<Input type="checkbox" id="volunteering" label="Do you also want to volunteer?" />
+
+				<button class="p-1 font-bold text-white w-full bg-theme-1 ">Hit the spam</button>
+			</span>
+		</form>
+	</div>
+</div>
