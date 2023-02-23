fc29b40d055ffbb5af2eff29f7b4dad59d138cad
qasimwarraich <qasim.warraich@gmail.com>
2023-02-23 16:58:16 +0100

    feat: Allow for management of checkbox "checked state" as a prop

--- src/lib/components/Input.svelte
+++ src/lib/components/Input.svelte
@@ -7,6 +7,7 @@
 	export let disabled = false;
 	export let required = false;
 	export let isCheckBox = false;
+	export let isChecked = false;
 
 	let normalField = 'border-theme-1 border-2 border-solid rounded-none w-full max-w-lg p-1';
 	let checkBoxField = 'ml-2 max-w-lg';
@@ -20,14 +21,28 @@
 	<label for={id} class="font-bold pb-1">
 		<span class="font-bold">{label}</span>
 	</label>
-	<input
-		class={isCheckBox ? checkBoxField : normalField}
-		{type}
-		{placeholder}
-		{required}
-		{disabled}
-		{id}
-		name={id}
-		{value}
-	/>
+	{#if !isCheckBox}
+		<input
+			class={isCheckBox ? checkBoxField : normalField}
+			{type}
+			{placeholder}
+			{required}
+			{disabled}
+			{id}
+			name={id}
+			{value}
+		/>
+	{:else}
+		<input
+			class={isCheckBox ? checkBoxField : normalField}
+            type="checkbox"
+			{placeholder}
+			{required}
+			{disabled}
+			{id}
+            bind:checked={isChecked}
+			name={id}
+			{value}
+		/>
+	{/if}
 </div>
