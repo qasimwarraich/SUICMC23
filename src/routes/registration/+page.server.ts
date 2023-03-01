b313b3489451ff51729d7a65bec6cbcbd09345f8
qasimwarraich <qasim.warraich@gmail.com>
2023-03-01 15:03:18 +0100

    fix: Fix type import issue in registration server file

--- src/routes/registration/+page.server.ts
+++ src/routes/registration/+page.server.ts
@@ -1,14 +1,11 @@
 import PocketBase from 'pocketbase';
 import { env } from '$env/dynamic/private';
-
-// TODO: FIX import error
 import { type RequestEvent } from '@sveltejs/kit';
 
 const pb = new PocketBase(env.BACKEND_URL);
 pb.autoCancellation(false);
 
 await pb.admins.authWithPassword(env.API_USER, env.API_TOKEN);
-
 
 export const actions = {
 	register: async ({ request }: RequestEvent) => {
