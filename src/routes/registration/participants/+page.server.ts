c52ef687ad54533e4a1c990c014aa3536243e575
qasimwarraich <qasim.warraich@gmail.com>
2023-03-02 16:57:43 +0100

    fix: Change participant page.ts to page.server.ts and remove `load` inheritence

---
+++ src/routes/registration/participants/+page.server.ts
@@ -0,0 +1,30 @@
+/** @type {import('./$types').PageLoad} */
+import PocketBase from 'pocketbase';
+import { env } from '$env/dynamic/private';
+
+const pb = new PocketBase(env.BACKEND_URL);
+pb.autoCancellation(false);
+
+await pb.admins.authWithPassword(env.API_USER, env.API_TOKEN);
+
+export const load = async () => {
+    const participants = await pb.collection(env.REGISTRATION_COLLECTION).getFullList();
+
+    const participantArray = [];
+
+    for (const p of participants) {
+        const res = {
+            first_name: p.first_name,
+            nick_name: p.nick_name,
+            race_number: p.race_number,
+            city: p.city,
+            category: p.category,
+        }
+        participantArray.push(res);
+    }
+    
+    return {
+        participants: participantArray
+    }
+}
+
