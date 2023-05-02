91a0301e760b3587c0888f31037262d77228090e
qasimwarraich <qasim.warraich@gmail.com>
2023-05-02 16:18:20 +0200

    feat: Make sponsor image hierarchy

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -13,7 +13,6 @@
 	import fizzen from '$lib/images/logos/fizzen.svg';
 	import gaskessel from '$lib/images/logos/gaskessel.png';
 	import gelateria from '$lib/images/logos/gelateria.svg';
-	import headdy from '$lib/images/logos/headdy.svg';
 	import herbaDiBerna from '$lib/images/logos/herba-di-berna.png';
 	import knog from '$lib/images/logos/knog.png';
 	import kryptonite from '$lib/images/logos/kryptonite.jpg';
@@ -22,7 +21,7 @@
 	import lola from '$lib/images/logos/lola.svg';
 	import noochFond from '$lib/images/logos/nooch-investment.png';
 	import nooch from '$lib/images/logos/nooch.png';
-	import oepfelchasper from '$lib/images/logos/oepfelchasper.jpg';
+	import oepfelchasper from '$lib/images/logos/oepfelchasper.png';
 	import saettaVerde from '$lib/images/logos/saetta-verde.svg';
 	import sbbLogistik from '$lib/images/logos/sbb-logistik.svg';
 	import sportamtbern from '$lib/images/logos/sportamtbern.svg';
@@ -72,126 +71,144 @@
 	<br />
 
 	<h2 class="my-10 text-theme-1 text-4xl">Our Sponsors!</h2>
-	<div class="flex  flex-row max-w-full flex-wrap justify-around">
-		<a href="https://7-druck.com" target="_blank" rel="noreferrer">
-			<img src={siebendruck} alt="7druck" />
-		</a>
-		<a href="https://adrianos.ch" target="_blank" rel="noreferrer">
-			<img src={adrianos} alt="adrianos" />
-		</a>
-		<a href="https://backup.ch" target="_blank" rel="noreferrer">
-			<img src={b1} alt="b1" />
-		</a>
-		<a href="https://bellocyclist.com" target="_blank" rel="noreferrer">
-			<img src={bello} alt="bello" />
-		</a>
-		<a href="https://bfh.ch" target="_blank" rel="noreferrer">
-			<img src={bfh} alt="bfh" />
-		</a>
-		<a href="https://cmiag.ch" target="_blank" rel="noreferrer">
-			<img src={cmi} alt="cmi" />
-		</a>
-		<a href="https://carvelo.ch" target="_blank" rel="noreferrer">
-			<img src={carvelo} alt="carvelo" />
-		</a>
-		<a href="https://dachstock.ch" target="_blank" rel="noreferrer">
-			<img src={dachstock} alt="dachstock" />
-		</a>
-		<a href="https://dirtysox.eu" target="_blank" rel="noreferrer">
-			<img src={dirtysox} alt="dirtysox" />
-		</a>
-		<a href="https://drahtesel.ch" target="_blank" rel="noreferrer">
-			<img src={drahtesel} alt="drahtesel" />
-		</a>
-		<a href="https://fizzen.ch" target="_blank" rel="noreferrer">
-			<img src={fizzen} alt="fizzen" />
-		</a>
-		<a href="https://gaskessel.ch" target="_blank" rel="noreferrer">
-			<img src={gaskessel} alt="gaskessel" />
-		</a>
-		<a href="https://gelateriadiberna.ch" target="_blank" rel="noreferrer">
-			<img src={gelateria} alt="gelateria" />
-		</a>
-		<a href="https://headdyoriginal.com" target="_blank" rel="noreferrer">
-			<img src={headdy} alt="headdy" />
-		</a>
-		<a href="https://herbadiberna.ch" target="_blank" rel="noreferrer">
-			<img src={herbaDiBerna} alt="herbaDiBerna" />
-		</a>
-		<a href="https://knog.com" target="_blank" rel="noreferrer">
-			<img src={knog} alt="knog" />
-		</a>
-		<a href="https://kryptonitelock.com" target="_blank" rel="noreferrer">
-			<img src={kryptonite} alt="kryptonite" />
-		</a>
-		<a href="https://kurierzentrale.ch" target="_blank" rel="noreferrer">
-			<img src={kuz} alt="kuz" />
-		</a>
-		<a href="https://lola.ch" target="_blank" rel="noreferrer">
-			<img src={lola} alt="lola" />
-		</a>
-		<a href="https://team-w.ch" target="_blank" rel="noreferrer">
-			<img src={ltw} alt="ltw" />
-		</a>
-		<a href="https://nooch.ch" target="_blank" rel="noreferrer">
-			<img src={nooch} alt="nooch" />
-		</a>
-		<a href="https://chistole.ch" target="_blank" rel="noreferrer">
-			<img src={noochFond} alt="noochFond" />
-		</a>
+	<div class="flex flex-row max-w-full flex-wrap justify-around ">
 		<a href="https://oepfelchasper.ch" target="_blank" rel="noreferrer">
-			<img src={oepfelchasper} alt="oepfelchasper" />
+			<img class="titanium" src={oepfelchasper} alt="oepfelchasper" />
 		</a>
-		<a href="https://saettaverde.ch" target="_blank" rel="noreferrer">
-			<img src={saettaVerde} alt="saettaverde" />
-		</a>
-		<a href="https://sbbcargo.com" target="_blank" rel="noreferrer">
-			<img src={sbbLogistik} alt="sbbLogistik" />
-		</a>
-		<a href="https://sportamt-bern.ch" target="_blank" rel="noreferrer">
-			<img src={sportamtbern} alt="sportamtbern" />
-		</a>
-		<a href="https://streit-ag.ch" target="_blank" rel="noreferrer">
-			<img src={streit} alt="streit" />
-		</a>
-		<a href="https://wir-bringens.ch" target="_blank" rel="noreferrer">
-			<img src={intact} alt="intact" />
+	</div>
+	<div class="flex flex-row max-w-full flex-wrap justify-around">
+		<a href="https://backup.ch" target="_blank" rel="noreferrer">
+			<img class="steel" src={b1} alt="b1" />
 		</a>
 		<a href="https://swissconnect.ch" target="_blank" rel="noreferrer">
-			<img src={swissconnect} alt="swissconnect" />
+			<img class="steel" src={swissconnect} alt="swissconnect" />
+		</a>
+	</div>
+	<div class="flex flex-row max-w-full flex-wrap justify-around">
+		<a href="https://7-druck.com" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={siebendruck} alt="7druck" />
+		</a>
+		<a href="https://adrianos.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={adrianos} alt="adrianos" />
+		</a>
+		<a href="https://bellocyclist.com" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={bello} alt="bello" />
+		</a>
+		<a href="https://bfh.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={bfh} alt="bfh" />
+		</a>
+		<a href="https://cmiag.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={cmi} alt="cmi" />
+		</a>
+		<a href="https://carvelo.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={carvelo} alt="carvelo" />
+		</a>
+		<a href="https://dachstock.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={dachstock} alt="dachstock" />
+		</a>
+		<a href="https://dirtysox.eu" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={dirtysox} alt="dirtysox" />
+		</a>
+		<a href="https://drahtesel.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={drahtesel} alt="drahtesel" />
+		</a>
+		<a href="https://fizzen.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={fizzen} alt="fizzen" />
+		</a>
+		<a href="https://gaskessel.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={gaskessel} alt="gaskessel" />
+		</a>
+		<a href="https://gelateriadiberna.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={gelateria} alt="gelateria" />
+		</a>
+		<a href="https://herbadiberna.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={herbaDiBerna} alt="herbaDiBerna" />
+		</a>
+		<a href="https://knog.com" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={knog} alt="knog" />
+		</a>
+		<a href="https://kryptonitelock.com" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={kryptonite} alt="kryptonite" />
+		</a>
+		<a href="https://kurierzentrale.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={kuz} alt="kuz" />
+		</a>
+		<a href="https://lola.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={lola} alt="lola" />
+		</a>
+		<a href="https://team-w.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={ltw} alt="ltw" />
+		</a>
+		<a href="https://nooch.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={nooch} alt="nooch" />
+		</a>
+		<a href="https://chistole.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={noochFond} alt="noochFond" />
+		</a>
+		<a href="https://oepfelchasper.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={oepfelchasper} alt="oepfelchasper" />
+		</a>
+		<a href="https://saettaverde.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={saettaVerde} alt="saettaverde" />
+		</a>
+		<a href="https://sbbcargo.com" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={sbbLogistik} alt="sbbLogistik" />
+		</a>
+		<a href="https://sportamt-bern.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={sportamtbern} alt="sportamtbern" />
+		</a>
+		<a href="https://streit-ag.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={streit} alt="streit" />
+		</a>
+		<a href="https://wir-bringens.ch" target="_blank" rel="noreferrer">
+			<img class="aluminium" src={intact} alt="intact" />
 		</a>
 		<a href="https://syndicom.ch" target="_blank" rel="noreferrer">
-			<img src={syndicom} alt="syndicom" />
+			<img class="aluminium" src={syndicom} alt="syndicom" />
 		</a>
 		<a href="https://untamed.love" target="_blank" rel="noreferrer">
-			<img src={untamed} alt="untamed" />
+			<img class="aluminium" src={untamed} alt="untamed" />
 		</a>
 		<a href="https://veloblitz.ch" target="_blank" rel="noreferrer">
-			<img src={veloblitz} alt="veloblitz" />
+			<img class="aluminium" src={veloblitz} alt="veloblitz" />
 		</a>
 		<a href="https://veloplus.ch" target="_blank" rel="noreferrer">
-			<img src={veloplus} alt="veloplus" />
+			<img class="aluminium" src={veloplus} alt="veloplus" />
 		</a>
 		<a href="https://velokurierbern.ch" target="_blank" rel="noreferrer">
-			<img src={vkb} alt="vkb" />
+			<img class="aluminium" src={vkb} alt="vkb" />
 		</a>
 		<a href="https://velokurierluzernzug.ch" target="_blank" rel="noreferrer">
-			<img src={vkluzern} alt="vkluzern" />
+			<img class="aluminium" src={vkluzern} alt="vkluzern" />
 		</a>
 		<a href="https://velokurier.sg" target="_blank" rel="noreferrer">
-			<img src={vksg} alt="vksg" />
+			<img class="aluminium" src={vksg} alt="vksg" />
 		</a>
 		<a href="https://velokurierwinterthur.ch" target="_blank" rel="noreferrer">
-			<img src={vkwinti} alt="vkwinti" />
+			<img class="aluminium" src={vkwinti} alt="vkwinti" />
 		</a>
 	</div>
 </div>
 
 <style>
-	img {
+	.aluminium {
 		padding: 1rem;
 		aspect-ratio: 1/1;
-		max-width: 200px;
+		max-width: 100px;
+		object-fit: contain;
+	}
+	.titanium {
+		padding: 0rem !important;
+		aspect-ratio: 1/1;
+		max-width: 300px !important;
+		min-width: 250px !important;
+		object-fit: contain;
+	}
+	.steel {
+		padding: 0rem !important;
+		aspect-ratio: 1/1;
+		max-width: 300px !important;
+		min-width: 200px;
 		object-fit: contain;
 	}
 	a {
