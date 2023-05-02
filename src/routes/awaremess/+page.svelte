30b9d6c7f9d6b2ec6895d8361c07291885f3632e
qasimwarraich <qasim.warraich@gmail.com>
2023-05-02 15:07:30 +0200

    feat: Add intial text to Awaremess page

--- src/routes/awaremess/+page.svelte
+++ src/routes/awaremess/+page.svelte
@@ -3,11 +3,63 @@
 </script>
 
 <svelte:head>
-	<title>SUICMC23 BERN: AwareMess</title>
+	<title>SUICMC23 BERN: Awaremess</title>
 	<meta name="description" content="Awaremess information SUICMC23" />
 </svelte:head>
 
 <h1 class="flex flex-col items-center justify-center text-5xl text-theme-1 font-semibold">
 	<img class="h-80" src={helmet} alt="SUICMC" />
-	Coming soon!
+	Awaremess
 </h1>
+
+<p class="py-4">
+	SUICMC23 Awaremess welcomes you whole heartedly to this event - we're looking forward to spending
+	a wonderful weekend filled with messfam, races, and events with all of you. The championship is
+	traditionally a gathering bringing together various social realities, opinions, and expectations.
+	To have a memorable time together, we find it crucial to set some expectations on how we would
+	like to treat each other. As we understand the well-being and safety of all people present to be
+	in our collective responsibility, we're inviting you to read this concept thoroughly and discuss
+	it with peers. And most importantly to hold each other accountable in creating more reliable safer
+	spaces.
+</p>
+
+<p class="py-4">
+	The Swiss Cycling Messenger Community is a predominantly white and cis-male and privileged group
+	of people. In order to promote solidarity and diversity within this wonderful community, we regard
+	it as essential to cultivate a culture of respect and awareness - for one another as well as for
+	our environment. We ask you to take notice of your surroundings and shared spaces, as well as the
+	people in it. Be aware of and respect other people's boundaries. Help promoting a culture of
+	consent. Be sensitive with each other and avoid assumptions about other people's experiences.
+	Especially regarding gender, race, sexuality, or (physical and mental) health.
+</p>
+
+<p class="py-4">
+	Discrimination of any kind is not welcome here. Because discriminative behaviour can be subtle,
+	it's our responsibility to pay attention to what and how we're saying and doing things. Before you
+	talk to a person check if they are up for it. If in doubt, ask them. Do not explain things without
+	being asked to and grow into a habit of asking questions.
+</p>
+
+<p class="py-4">It's out responsibility to create accessible spaces and support structures.</p>
+
+<p class="py-4">
+	In whatever case you need support, assistance or anyone to talk to, Awaremess volunteers will be
+	present during all the event. We have a phone number you can contact at any time and we support
+	you within the scope of our possibilities.
+</p>
+
+<p class="py-4">
+	If you notice someone seeming uncomfortable or harassed, try to support the concerned person i.e.
+	going up to them and offering assistance if needed. Be considerate with each other and take
+	responsibility, we're all part of the environment we'd like to create.
+</p>
+
+<p class="py-4">
+	With all of this, we hope to create an inclusive, supportive, and respectful event together. If
+	you have any questions or constructive feedback about this concept, please let us know. Also, are
+	we still looking for people who would like to cover Awaremess shifts during the event. Please
+	contact us if you'd like to get involved (<a href="mailto:awaremess@suicmc23.ch">
+		awaremess@suicmc23.ch</a
+	>).
+</p>
+<p class="py-4">See you around ❤️ , <br /> Awaremess SUICMC23 Bern</p>
