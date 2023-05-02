5b5f47122b27f98c2bf9711e7e84ac5f837d1b73
qasimwarraich <qasim.warraich@gmail.com>
2023-05-02 13:25:24 +0200

    feat: Add sponsor images and links

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,5 +1,33 @@
 <script lang="ts">
 	import train from '$lib/images/train.png';
+	import adrianos from '$lib/images/logos/adrianos-coffee.png';
+	import b1 from '$lib/images/logos/backup-one.png';
+	import bfh from '$lib/images/logos/bfh.svg';
+	import carvelo from '$lib/images/logos/carvelo.png';
+	import cmi from '$lib/images/logos/cmi.png';
+	import drahtesel from '$lib/images/logos/drahtesel.svg';
+	import fizzen from '$lib/images/logos/fizzen.svg';
+	import headdy from '$lib/images/logos/headdy.svg';
+	import herbaDiBerna from '$lib/images/logos/herba-di-berna.png';
+	import knog from '$lib/images/logos/knog.png';
+	import kryptonite from '$lib/images/logos/kryptonite.jpg';
+	import kuz from '$lib/images/logos/kuz.png';
+	import ltw from '$lib/images/logos/ltw.svg';
+	import lola from '$lib/images/logos/lola.svg';
+	import noochFond from '$lib/images/logos/nooch-investment.png';
+	import nooch from '$lib/images/logos/nooch.png';
+	import oepfelchasper from '$lib/images/logos/oepfelchasper.jpg';
+	import sbbLogistik from '$lib/images/logos/sbb-logistik.svg';
+	import sportamtbern from '$lib/images/logos/sportamtbern.svg';
+	import swissconnect from '$lib/images/logos/swissconnect.svg';
+	import syndicom from '$lib/images/logos/syndicom.svg';
+	import untamed from '$lib/images/logos/untamed.svg';
+	import veloblitz from '$lib/images/logos/veloblitz.png';
+	import veloplus from '$lib/images/logos/veloplus.png';
+	import vkb from '$lib/images/logos/vkb.svg';
+	import vkluzern from '$lib/images/logos/vkluzern.png';
+	import vksg from '$lib/images/logos/vksg.svg';
+	import vkwinti from '$lib/images/logos/vkwinti.svg';
 </script>
 
 <svelte:head>
@@ -9,9 +37,108 @@
 
 <div class="flex flex-col items-center justify-center text-5xl text-theme-1 font-semibold">
 	<img class="h-80" src={train} alt="SUICMC" />
-	<h1>Coming soon!</h1>
+	<h1>More Info coming soon!</h1>
 
-	<h2 class="my-10">
+
+	<h2 class="my-10 text-theme-1 text-4xl">Payment information</h2>
+	<h2 class="">
 		Registration payment information can be found <a href={'/cream'}>here.</a>
 	</h2>
+
+	<h2 class="my-10 text-theme-1 text-4xl">Our Sponsors!</h2>
+	<div class="flex  flex-row max-w-full flex-wrap justify-around">
+		<a href="https://adrianos.ch" target="_blank" rel="noreferrer">
+			<img src={adrianos} alt="adrianos" />
+		</a>
+		<a href="https://backup.ch" target="_blank" rel="noreferrer">
+			<img src={b1} alt="b1" />
+		</a>
+		<a href="https://bfh.ch" target="_blank" rel="noreferrer">
+			<img src={bfh} alt="bfh" />
+		</a>
+		<a href="https://cmiag.ch" target="_blank" rel="noreferrer">
+			<img src={cmi} alt="cmi" />
+		</a>
+		<a href="https://drahtesel.ch" target="_blank" rel="noreferrer">
+			<img src={drahtesel} alt="drahtesel" />
+		</a>
+		<a href="https://fizzen.ch" target="_blank" rel="noreferrer">
+			<img src={fizzen} alt="fizzen" />
+		</a>
+		<a href="https://headdyoriginal.com" target="_blank" rel="noreferrer">
+			<img src={headdy} alt="headdy" />
+		</a>
+		<a href="https://herbadiberna.ch" target="_blank" rel="noreferrer">
+			<img src={herbaDiBerna} alt="herbaDiBerna" />
+		</a>
+		<a href="https://knog.com" target="_blank" rel="noreferrer">
+			<img src={knog} alt="knog" />
+		</a>
+		<a href="https://kryptonitelock.com" target="_blank" rel="noreferrer">
+			<img src={kryptonite} alt="kryptonite" />
+		</a>
+		<a href="https://kurierzentrale.ch" target="_blank" rel="noreferrer">
+			<img src={kuz} alt="kuz" />
+		</a>
+		<a href="https://lola.ch" target="_blank" rel="noreferrer">
+			<img src={lola} alt="lola" />
+		</a>
+		<a href="https://team-w.ch" target="_blank" rel="noreferrer">
+			<img src={ltw} alt="ltw" />
+		</a>
+		<a href="https://nooch.ch" target="_blank" rel="noreferrer">
+			<img src={nooch} alt="nooch" />
+		</a>
+		<a href="https://sbb.ch" target="_blank" rel="noreferrer">
+			<img src={sbbLogistik} alt="sbbLogistik" />
+		</a>
+		<a href="https://oepfelchasper.ch" target="_blank" rel="noreferrer">
+			<img src={oepfelchasper} alt="oepfelchasper" />
+		</a>
+		<a href="https://chistole.ch" target="_blank" rel="noreferrer">
+			<img src={noochFond} alt="noochFond" />
+		</a>
+		<a href="https://sportamt-bern.ch" target="_blank" rel="noreferrer">
+			<img src={sportamtbern} alt="sportamtbern" />
+		</a>
+		<a href="https://swissconnect.ch" target="_blank" rel="noreferrer">
+			<img src={swissconnect} alt="swissconnect" />
+		</a>
+		<a href="https://syndicom.ch" target="_blank" rel="noreferrer">
+			<img src={syndicom} alt="syndicom" />
+		</a>
+		<a href="https://untamed.love" target="_blank" rel="noreferrer">
+			<img src={untamed} alt="untamed" />
+		</a>
+		<a href="https://veloblitz.ch" target="_blank" rel="noreferrer">
+			<img src={veloblitz} alt="veloblitz" />
+		</a>
+		<a href="https://veloplus.ch" target="_blank" rel="noreferrer">
+			<img src={veloplus} alt="veloplus" />
+		</a>
+		<a href="https://velokurierbern.ch" target="_blank" rel="noreferrer">
+			<img src={vkb} alt="vkb" />
+		</a>
+		<a href="https://velokurierluzernzug.ch" target="_blank" rel="noreferrer">
+			<img src={vkluzern} alt="vkluzern" />
+		</a>
+		<a href="https://velokurier.sg" target="_blank" rel="noreferrer">
+			<img src={vksg} alt="vksg" />
+		</a>
+		<a href="https://velokurierwinterthur.ch" target="_blank" rel="noreferrer">
+			<img src={vkwinti} alt="vkwinti" />
+		</a>
+	</div>
 </div>
+
+<style>
+	img {
+		padding: 1rem;
+		aspect-ratio: 1/1;
+		max-width: 200px;
+		object-fit: contain;
+	}
+	a {
+		display: contents;
+	}
+</style>
