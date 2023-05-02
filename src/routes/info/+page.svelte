ac958b778e69cc68883d21bceb8bbb8608e492c3
qasimwarraich <qasim.warraich@gmail.com>
2023-05-02 13:41:44 +0200

    fix: Fix typo

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -39,7 +39,7 @@
 
 <div class="flex flex-col items-center justify-center text-5xl text-theme-1 font-semibold">
 	<img class="h-80" src={train} alt="SUICMC" />
-	<h1>More Info coming soon!</h1>
+	<h1>More info coming soon!</h1>
 
 
 	<h2 class="my-10 text-theme-1 text-4xl">Payment information</h2>
