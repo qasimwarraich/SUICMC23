ed46a492fcc8acbfac582405dc5f6b3790615e49
qasimwarraich <qasim.warraich@gmail.com>
2023-05-10 16:53:55 +0200

    feat: Create shell of results table

---
+++ src/lib/data/results.ts
@@ -0,0 +1,88 @@
+export const data = `
+[
+{
+  "name": "Theodor",
+  "position": 1,
+  "points": 10,
+  "race_number": 420,
+  "team": "VKB",
+  "city": "Bern",
+  "pronoun": "er",
+  "gear": "fixed",
+  "bike": "tractor",
+  "age": "20-30"
+},
+{
+  "name": "Theodora",
+  "position": 2,
+  "points": 8,
+  "race_number": 161,
+  "team": "velocité",
+  "city": "Lausanne",
+  "pronoun": "sie",
+  "gear": "geared",
+  "bike": "carbon",
+  "age": "20-30"
+},
+{
+  "name": "Debora",
+  "position": 3,
+  "points": 8,
+  "race_number": 1312,
+  "team": "velocité",
+  "city": "Lausanne",
+  "pronoun": "n/a",
+  "gear": "fixed",
+  "bike": "steel",
+  "age": "20-30"
+},
+{
+  "name": "Paul",
+  "position": 4,
+  "points": 7,
+  "race_number": 4,
+  "team": "velocité",
+  "city": "Lausanne",
+  "pronoun": "sie",
+  "gear": "fixed",
+  "bike": "steel",
+  "age": "30-40"
+},
+{
+  "name": "Fritz",
+  "position": 5,
+  "points": 2,
+  "race_number": 20,
+  "team": "velocité",
+  "city": "Lausanne",
+  "pronoun": "sie",
+  "gear": "fixed",
+  "bike": "tractor",
+  "age": "30-40"
+},
+{
+  "name": "Fritza",
+  "position": 6,
+  "points": 1,
+  "race_number": 13,
+  "team": "velocité",
+  "city": "Lausanne",
+  "pronoun": "none",
+  "gear": "fixed",
+  "bike": "steel",
+  "age": "30-40"
+},
+{
+  "name": "Manfred",
+  "position": 7,
+  "points": 1,
+  "race_number": 12,
+  "team": "velocité",
+  "city": "Lausanne",
+  "pronoun": "n/a",
+  "gear": "fixed",
+  "bike": "aluminium",
+  "age": "30-40"
+}
+]
+`;
