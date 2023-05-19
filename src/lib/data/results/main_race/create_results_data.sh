4f7b0e4dfaaa12ac97241fbf7022900e52c0a0a6
qasimwarraich <qasim.warraich@gmail.com>
2023-05-19 14:24:20 +0200

    chore: Update directory structure to be result specific

--- src/lib/data/results/main_race/create_results_data.sh
+++ src/lib/data/results/main_race/create_results_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mlr --icsv --ojson sort -nr points suicmc23results.csv > results.json
+mlr --icsv --ojson sort -nr points -n time  suicmc23results.csv > results.json
 
 echo "export const data = \`" > results.ts
 cat results.json >> results.ts
