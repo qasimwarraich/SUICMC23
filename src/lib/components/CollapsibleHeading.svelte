0c4008683cc3b42325ed092e9dc123b9176860ae
qasimwarraich <qasim.warraich@gmail.com>
2023-05-19 09:42:11 +0200

    feat: Add `CollapsibleHeading` component.

---
+++ src/lib/components/CollapsibleHeading.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { slide } from 'svelte/transition';
+	import Fa6RegularEye from '~icons/fa6-regular/eye';
+	import Fa6RegularEyeSlash from '~icons/fa6-regular/eye-slash';
+
+	export let title = 'title';
+
+	let showInformation = false;
+</script>
+
+<button on:click={() => (showInformation = !showInformation)}>
+	<h2 class="my-5 text-theme-1 text-4xl">
+		<div class="flex space-between">
+			<div class="px-2">{title}</div>
+			{#if showInformation}
+				<Fa6RegularEye class="text-lg" />
+			{:else}
+				<Fa6RegularEyeSlash class="text-lg" />
+			{/if}
+		</div>
+	</h2>
+</button>
+{#if showInformation}
+	<div transition:slide>
+		<slot />
+	</div>
+{/if}
