22409e5d1b53255a3476ae8cb2c061530be5fd02
qasimwarraich <qasim.warraich@gmail.com>
2023-05-26 12:30:33 +0200

    feat: Prize info updated

---
+++ src/routes/results/PrizeInfo.svelte
@@ -0,0 +1,25 @@
+<h2>
+	<div class="py-4">
+		Information on the prize ceremony: The usual categories (Open, WTNB+) have not been used for
+		this event. This is the result of a collective decision by the organising committee. We will
+		inform you in detail about the process of this decision in detail on our website. Furthermore,
+		our prize ceremony is of an exceptional kind, as we have only one unified results list, and we
+		do not want to shower the top ranks with prizes.
+
+		<br /><br />
+		Our main race team was given the task of designing a race that was as balanced as possible, where
+		all people should be equal with their physical conditions, in terms of biological sex. You will have
+		successfully participated in this experiment, and we are open hearing your feedback and input!
+
+		<br /><br />
+		With our general list of results, we want to promote an event culture where you can measure yourself
+		with other people in the courier scene, instead of trying to be the best of the scene. That's why
+		you will be able to highlight the results by e.g. age, fixed or team on our website. With this design,
+		you will be able to interpret the results as they seem appropriate for you and measure yourself against
+		the people you feel comfortable with.
+
+		<br /><br />
+		We don't reward first places, the first places on the list get to take home fame and glory.
+	</div>
+	<hr />
+</h2>
